b0086851e2c2ec38c3402ff93a45b97521474b4a
mily datta <dmily270@gmail.com>
2021-07-27 18:00:29 +0600

    some update

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,6 +4,11 @@ const Orders = () => {
     return (
         <div>
             <h1>This is orders</h1>
+            <p>lorem ipsum dolor sit am
+            lorem ipsum dolor sit am
+            lorem ipsum dolor sit am
+            lorem ipsum dolor sit am
+            </p>
         </div>
     );
 };
